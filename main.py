8b280c2f8c991a0eb1a490223eddb9ad5b7866b0
Shubham Bhardwaj <94232997+bhardwaj-shubham@users.noreply.github.com>
2023-11-14 11:51:18 +0530

    Error message added and change styles

--- main.py
+++ main.py
@@ -22,17 +22,17 @@ def get_weather():
 @app.route('/', methods=['POST'])
 def find_weather():
     city_name = request.form['city']
+    current_date = datetime.date.today().strftime("%d-%m-%Y")
 
     response = requests.get(
         f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric")
 
     if response.status_code != 200:
-        return {"error": "City not found"}
+        return render_template('index.html', current_date=current_date, error_message="City not found")
 
     data = response.json()
     icon_code = data['weather'][0]['icon']
     icon_url = "https://openweathermap.org/img/w/" + icon_code + ".png";
     weather_data = {'city': str(data['name'], ), 'temperature': str(data['main']['temp'], ),
                     'description': str(data['weather'][0]['description'])}
-    current_date = datetime.date.today().strftime("%d-%m-%Y")
     return render_template('index.html', current_date=current_date, weather_data=weather_data, icon_url=icon_url)
